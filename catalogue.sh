dd071e5c8f4272d60772ecff8c498f2f62f31e4c
Aquib Javed <aquib.javed1234@gmail.com>
2023-01-06 00:42:11 +0400

    Catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -7,8 +7,9 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 
 #useradd roboshop
-mkdir /app
+mkdir -p /app
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip
 
