62a08f26a031a223ae12cf942cfc879a03e0a02a
Aquib Javed <aquib.javed1234@gmail.com>
2023-01-13 01:18:17 +0400

    special vars

--- catalogue.sh
+++ catalogue.sh
@@ -9,54 +9,57 @@ yum install nodejs -y &>>${lOG}
 status_check
 
 print_head "Add Application User"
-useradd roboshop &>>${lOG}
+id roboshop &>>${lOG}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${lOG}
+fi
 status_check
 
 mkdir -p /app &>>${lOG}
 status_check
 
-echo -e "\e[35m Downloading App Content\e[0m"
+print_head "Downloading App Content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${lOG}
 status_check
 
-echo -e "\e[35m Clean up Old Content\e[0m"
+print_head "Clean up Old Content"
 rm -rf /app/* &>>${lOG}
 status_check
 
-echo -e "\e[35m Extracting App Content\e[0m"
+print_head "Extracting App Content"
 cd /app
 unzip /tmp/catalogue.zip &>>${lOG}
 status_check
 
-echo -e "\e[35m Installing NodeJS Dependencies\e[0m"
+print_head "Installing NodeJS Dependencies"
 cd /app &>>${lOG}
 npm install &>>${lOG}
 status_check
 
-echo -e "\e[35m Configuring Catalogue Service File\e[0m"
+print_head "Configuring Catalogue Service File"
 cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${lOG}
 status_check
 
-echo -e "\e[35m Reload SystemD\e[0m"
+print_head "Reload SystemD"
 systemctl daemon-reload &>>${lOG}
 status_check
 
-echo -e "\e[35m Enable Catalogue Service\e[0m"
+print_head "Enable Catalogue Service"
 systemctl enable catalogue &>>${lOG}
 status_check
 
-echo -e "\e[35m Start Catalogue Service\e[0m"
+print_head "Start Catalogue Service"
 systemctl start catalogue &>>${lOG}
 status_check
 
-echo -e "\e[35m Configuring Mongo Repo\e[0m"
+print_head "Configuring Mongo Repo"
 cp ${script_location/}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${lOG}
 status_check
 
-echo -e "\e[35m Install Mongo Client\e[0m"
+print_head "Install Mongo Client"
 yum install mongodb-org-shell -y &>>${lOG}
 status_check
 
-echo -e "\e[35m Load Schema\e[0m"
+print_head "Load Schema"
 mongo --host mongodb-dev.aquibdevops.online </app/schema/catalogue.js &>>${lOG}
 status_check
