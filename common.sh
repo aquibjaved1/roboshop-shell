f8a37eaf9dc04ca09e9be3bef04d0039bbc3ea22
Aquib Javed <aquib.javed1234@gmail.com>
2023-01-15 01:25:55 +0400

    redis

--- common.sh
+++ common.sh
@@ -15,3 +15,51 @@ status_check() {
  print_head() {
      echo -e "\e[1m $1 \e[0m"
  }
+
+ NODEJS() {
+   print_head "Downloading App Content"
+   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${lOG}
+   status_check
+
+   print_head "Clean up Old Content"
+   rm -rf /app/* &>>${lOG}
+   status_check
+
+   print_head "Extracting App Content"
+   cd /app
+   unzip /tmp/{component}.zip &>>${lOG}
+   status_check
+
+   print_head "Installing NodeJS Dependencies"
+   cd /app &>>${lOG}
+   npm install &>>${lOG}
+   status_check
+
+   print_head "Configuring ${component} Service File"
+   cp ${script_location}/files/${component}.service /etc/systemd/system/catalogue.service &>>${lOG}
+   status_check
+
+   print_head "Reload SystemD"
+   systemctl daemon-reload &>>${lOG}
+   status_check
+
+   print_head "Enable ${component} Service"
+   systemctl enable ${component} &>>${lOG}
+   status_check
+
+   print_head "Start ${component} Service"
+   systemctl start ${component} &>>${lOG}
+   status_check
+
+   print_head "Configuring Mongo Repo"
+   cp ${script_location/}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${lOG}
+   status_check
+
+   print_head "Install Mongo Client"
+   yum install mongodb-org-shell -y &>>${lOG}
+   status_check
+
+   print_head "Load Schema"
+   mongo --host mongodb-dev.aquibdevops.online </app/schema/catalogue.js &>>${lOG}
+   status_check
+ }
